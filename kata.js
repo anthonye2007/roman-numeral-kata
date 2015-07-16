7772026b9988c32eb06f3269580b619ca718f8a0
Anthony Elliott <Anthony.Elliott@inin.com>
2015-07-16 12:26:50 -0400

    Hello world

--- kata.js
+++ kata.js
@@ -1,1 +1,12 @@
 // http://agilekatas.co.uk/katas/romannumerals-kata
+
+function Greeter() {
+  function sayHello() {
+    return "Hello"
+  }
+
+  this.sayHello = sayHello;
+}
+
+var greeter = new Greeter();
+console.log(greeter.sayHello())
